fd078e921555268af527d0c71b19657127eb3d17
foxyq <foxyzv@gmail.com>
2017-09-28 15:28:15 +0200

    api done

---
+++ README.md
@@ -0,0 +1,6 @@
+# loans_api
+homework / backend part
+
+1. git clone
+2. (npm install)
+3. node server.js
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const readline = require("readline");
+const port = 8082;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+router.post("/loan", (req, res) => {
+  const price = req.body.price;
+  const months = req.body.months;
+  const insurance = req.body.insurance;
+
+  const lowerRate = 0.069;
+  const higherRate = 0.0711;
+
+  let rate = 0;
+
+  if (insurance === true) {
+    rate = higherRate;
+  } else {
+    rate = lowerRate;
+  }
+
+  let result =
+    price * (rate / 12 / (1 - Math.pow(1 / (1 + rate / 12), months))) * months;
+
+  res.json(result);
+});
+
+app.use("/api", router);
+app.listen(port);
+console.log("Magic happens on port " + port);
